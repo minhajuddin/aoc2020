bd56dac12c7fb409d1bd07d7dd64ec261879c876
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2020-12-02 00:45:12 -0500

    Day2 Part2

--- ruby/day2/app.rb
+++ ruby/day2/app.rb
@@ -1,15 +1,30 @@
+#!/usr/bin/env ruby
 
-def parse_rule(rule)
+# Run as below:
+# ruby app.rb < input
+
+def parse(line)
+  rule, password = line.split(":").map(&:strip)
   range, char = rule.split(' ')
   min, max = range.split('-').map(&:to_i)
-  [min..max, char]
+  [min, max, char, password]
 end
 
-def valid?(line)
-  rule, password = line.split(":").map(&:strip)
-  range, char = parse_rule(rule)
+def valid_part2?(line)
+  pos1, pos2, char, password = parse(line)
+  (password[pos1 - 1] == char) ^ (password[pos2 - 1] == char)
+end
+
+def valid_part1?(line)
+  min, max, char, password = parse(line)
   char_count = password.each_char.count {|x| x == char} 
-  range.include?(char_count)
+  (min..max).include?(char_count)
 end
 
-puts ARGF.each_line.find_all{|x| valid?(x)}.count
+def valid_password_count(lines, validator)
+  lines.find_all{|x| validator.call(x)}.count
+end
+
+lines = ARGF.each_line.to_a
+puts "Part1: #{valid_password_count(lines, method(:valid_part1?))}"
+puts "Part2: #{valid_password_count(lines, method(:valid_part2?))}"
