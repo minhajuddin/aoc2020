8a9af9e76b01bb0cb3a6d7985f7497dfa0a88c9a
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2020-12-03 00:15:22 -0500

    Day 3 Part 2

--- ruby/day3/app.rb
+++ ruby/day3/app.rb
@@ -2,17 +2,25 @@
 
 # day3 solution
 input_file_path = File.expand_path("../../../inputs/day3", __FILE__)
-lines = File.readlines(input_file_path).map(&:strip)
+$lines = File.readlines(input_file_path).map(&:strip)
 
 # your solution
-tree_count = 0
-x = 0
-(0..(lines.length-1)).each do |y|
-  line = lines[y]
-  tree_count += 1 if line[x] == "#"
-  #puts "#{y}\t#{x}\t#{line[x]}"
-  x += 3
-  x %= line.length
+def tree_count(dx, dy)
+  tree_count = 0
+  x = 0
+  y = 0
+
+  loop do
+    line = $lines[y]
+    return tree_count unless line
+
+    tree_count += 1 if line[x] == "#"
+
+    #puts "#{y}\t#{x}\t#{line[x]}"
+    x += dx
+    x %= line.length
+    y += dy
+  end
 end
 
-puts tree_count
+puts tree_count(1, 1) * tree_count(3, 1) * tree_count(5, 1) * tree_count(7, 1) * tree_count(1, 2)
