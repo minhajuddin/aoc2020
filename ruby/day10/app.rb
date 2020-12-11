ea6de5c6b8af02bd72bcb6035ca8245bf39495af
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2020-12-10 22:41:15 -0500

    Part 2 failed attempts

--- ruby/day10/app.rb
+++ ruby/day10/app.rb
@@ -5,17 +5,17 @@ input_file_path = File.expand_path("../../../inputs/day10", __FILE__)
 
 # your solution
 example =<<~EX.lines.map(&:strip).map(&:to_i)
-16
-10
-15
-5
 1
-11
-7
-19
-6
-12
 4
+5
+6
+7
+10
+11
+12
+15
+16
+19
 EX
 
 example2 =<<~EX.lines.map(&:strip).map(&:to_i)
@@ -67,11 +67,98 @@ def count_diff(inputs)
   end
 end
 
-inputs = layout_adapters(example)
-diffs = count_diff(inputs)
+# part 1
+#inputs = layout_adapters(example)
+#diffs = count_diff(inputs)
 
-puts diffs
-puts diffs[1] * diffs[3]
+#puts diffs
+#puts diffs[1] * diffs[3]
+
+#inputs = layout_adapters(example2)
+#diffs = count_diff(inputs)
+
+#puts diffs
+#puts diffs[1] * diffs[3]
+
+#inputs = File.readlines(input_file_path).map{|x| x.strip.to_i}
+#inputs = layout_adapters(inputs)
+#diffs = count_diff(inputs)
+
+#puts diffs
+#puts diffs[1] * diffs[3]
+
+def count_possibilities(inputs)
+  inputs = layout_adapters(inputs)
+  first = 0
+  last = inputs.max
+
+  curr = first
+  counts = []
+
+  #0-3: 0-1  => 1
+  #1-4: 1-4  => 1
+  #4-7: 4567 457 467 47 => 4
+  #7-10: 7-10 => 1
+  #10-13: 10 11 12, 10 12 => 2
+  #12-15: 12-15 : 1
+  #15:18: 15-16: 1
+  #16:19: 16-19: 1
+  #19:21: 19-21: 1
+
+  possibilities = ->(inputs) do
+    {
+      4 => 4, # 4567, 4 inputs have 4 ways of being laid out: 4567, 457, 467, 47
+      3 => 2, # 457, 3 inputs have 2 ways of being laid out: 457, 47
+      2 => 1 # 47, 2 inputs have 1 way of being laid out: 47
+    }[inputs.count]
+  end
+
+  loop do
+    next3 = inputs.find_all{|x| x >= curr && x <= (curr + 3)}
+    puts ">>>NEXT3: (#{curr}, #{next3.max}) #{next3.inspect}"
+    counts.push possibilities.(next3)
+    curr = next3.max
+
+    break if curr == last
+  end
+
+  #puts counts.inspect
+  counts.reduce(1, &:*)
+end
+
+def valid?(combo)
+  puts "validating #{ combo.inspect }"
+  !combo.sort.each_cons(2).any?{|a, b| (b - a) > 3}
+end
+
+def count_possibilities2(inputs)
+  inputs = inputs.sort
+  oj = 0 # outlet jolt
+  iaj = inputs.max + 3 # input adapter jolt
+
+  (1..inputs.length).map do |adapter_count|
+    inputs.combination(adapter_count).count do |combo|
+      valid?(combo << oj << iaj)
+    end
+  end.sum
+end
+
+# puts count_possibilities2(example)
+#puts count_possibilities2(example2)
+#puts count_possibilities(example2)
+
+
+#def combinations(inputs, start_j, end_j)
+  #cs = inputs.find_all{|x| (start_j..end_j).include?(x)}
+  #puts cs.inspect
+  #cs.count
+#end
+
+#cs = steps.each_cons(2).map do |a, b|
+  #combinations(inputs, a, b)
+#end
+
+#puts cs.map{|x| x}.inspect
 
 inputs = layout_adapters(example2)
 diffs = count_diff(inputs)
@@ -79,9 +166,3 @@ diffs = count_diff(inputs)
 puts diffs
 puts diffs[1] * diffs[3]
 
-inputs = File.readlines(input_file_path).map{|x| x.strip.to_i}
-inputs = layout_adapters(inputs)
-diffs = count_diff(inputs)
-
-puts diffs
-puts diffs[1] * diffs[3]
